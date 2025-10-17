111e5f690fbb69d20500952cc860d5559a15a493
livstsh <drkamran185@gmail.com>
2025-10-18 00:02:22 +0500

    Update main-ping.js

--- plugins/main-ping.js
+++ plugins/main-ping.js
@@ -2,7 +2,7 @@ const config = require('../config');
 const { cmd, commands } = require('../command');
 
 cmd({
-    pattern: "ping",
+    pattern: "ping5",
     alias: ["speed","pong"],use: '.ping',
     desc: "Check bot's response time.",
     category: "main",
@@ -57,7 +57,7 @@ async (conn, mek, m, { from, quoted, sender, reply }) => {
 // ping2 
 
 cmd({
-    pattern: "ping2",
+    pattern: "ping4",
     desc: "Check bot's response time.",
     category: "main",
     react: "🍂",
