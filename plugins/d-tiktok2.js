4888943d9e37e850f01f57f2b7f7125c10ea2698
livstsh <drkamran185@gmail.com>
2025-10-28 20:28:37 +0500

    Update d-tiktok2.js

--- plugins/d-tiktok2.js
+++ plugins/d-tiktok2.js
@@ -16,7 +16,7 @@ async (conn, mek, m, { from, args, q, reply }) => {
         
         reply("Downloading video, please wait...");
         
-        const apiUrl = `https://jawad-tech.vercel.app/download/tiktok?url=${q}`;
+        const apiUrl = `https://delirius-apiofc.vercel.app/download/tiktok?url=${q}`;
         const { data } = await axios.get(apiUrl);
         
         if (!data.status || !data.data) return reply("Failed to fetch TikTok video.");
