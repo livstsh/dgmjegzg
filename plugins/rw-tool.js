45c5e544f3fedd291ed49fe913041f55ece05c7a
livstsh <drkamran185@gmail.com>
2025-10-29 14:52:46 +0500

    Update rw-tool.js

--- plugins/rw-tool.js
+++ plugins/rw-tool.js
@@ -17,7 +17,7 @@ cmd({
     const { data } = await axios.get(apiUrl);
     
     if (data.status && data.imgUrl) {
-      const caption = `🌌 *Random Wallpaper: ${query}*\n\n> *Dua Fatima*`;
+      const caption = `🌌 *Random Wallpaper: ${query}*\n\n> *DR KAMRAN*`;
       await conn.sendMessage(from, { image: { url: data.imgUrl }, caption }, { quoted: m });
     } else {
       reply(`❌ No wallpaper found for *"${query}"*.`);
