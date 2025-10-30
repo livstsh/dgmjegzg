143e7dea625ee54cdbb88ee48d71f653a14199f5
livstsh <drkamran185@gmail.com>
2025-10-30 07:04:46 +0500

    Update download.js

--- plugins/download.js
+++ plugins/download.js
@@ -182,7 +182,7 @@ cmd({
       react: { text: "⏳", key: m.key }
     });
 
-    const response = await axios.get(`https://www.dark-yasiya-api.site/download/mfire?url=${q}`);
+    const response = await axios.get(`https://api.nekolabs.web.id/downloader/mediafire?url=${q}`);
     const data = response.data;
 
     if (!data || !data.status || !data.result || !data.result.dl_link) {
