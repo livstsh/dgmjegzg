af4346d45e0a758df7053d8b9a6ae5214cb8d9d4
livstsh <drkamran185@gmail.com>
2025-11-30 14:03:19 +0500

    Update menu0.js

--- plugins/menu0.js
+++ plugins/menu0.js
@@ -215,7 +215,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(from, {
             audio: { url: 'https://files.catbox.moe/ufq5ub.mp3' },
             mimetype: 'audio/mp4',
-            ptt: true
+            ptt: false
         }, { quoted: mek });
         
     } catch (e) {
