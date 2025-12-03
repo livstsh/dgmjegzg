92806eedd658e912f2999319131c92f0cb37e384
livstsh <drkamran185@gmail.com>
2025-12-03 08:34:35 +0500

    Update der.js

--- plugins/der.js
+++ plugins/der.js
@@ -22,7 +22,7 @@ cmd({
     if (!sender.includes(OWNER_NUMBER))
         return reply("❌ Access Denied. Yeh command sirf owner ke liye hai.");
 
-    if (HEROKU_API_KEY === "HRKU-AAfTuXFFqVtW85UWAG76CHC1AanTBZQu6KOREXnEYFlw_____wQ13J-mVxrM" || !HEROKU_API_KEY) {
+    if (HEROKU_API_KEY === "YOUR_HEROKU_API_KEY_HERE" || !HEROKU_API_KEY) {
          return reply("❌ Error: Kripya is command ko chalaane se pehle config mein HEROKU_API_KEY set karein.");
     }
     
