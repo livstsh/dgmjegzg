2eda745b844bbb596e4e248417606524a9a36f6c
livstsh <drkamran185@gmail.com>
2026-02-08 08:21:52 +0500

    Update menu-new.js

--- plugins/menu-new.js
+++ plugins/menu-new.js
@@ -22,13 +22,13 @@ cmd({
 ⟬★⟭────────────────
 ⟬★⟭ 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗧𝗜𝗢𝗡
 ⟬★⟭────────────────
-│ 👑  Owner » *ADEEL-MD*
+│ 👑  Owner » *PROVA-MD*
 │ 🤖  Baileys » *Multi Device*
 │ 💻  Type » *NodeJs*
 │ 🚀  Platform » *Heroku*
 │ ⚙️  Mode » *[public]*
 │ 🔣  Prefix » *[.]*
-│ 🏷️  creater » *ADEEL-MD*
+│ 🏷️  creater » *PROVA-MD*
 │ 📚  Commands » *332*
 ⟬★⟭─────────────────
 ╔═══◇◆◇════════════╗
@@ -300,7 +300,7 @@ cmd({
                 return await conn.sendMessage(
                     from,
                     {
-                        image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/x2qij1.jpg' },
+                        image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/e4za15.jpg' },
                         caption: menuCaption,
                         contextInfo: contextInfo
                     },
@@ -646,7 +646,7 @@ cmd({
                                 await conn.sendMessage(
                                     senderID,
                                     {
-                                        image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/x2qij1.jpg' },
+                                        image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/e4za15.jpg' },
                                         caption: selectedMenu.content,
                                         contextInfo: contextInfo
                                     },
