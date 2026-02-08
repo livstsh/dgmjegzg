30edc937b2d5cdc200ea13fcc3dbe8560fda927b
livstsh <drkamran185@gmail.com>
2026-02-08 09:59:09 +0500

    Update privacy-tool.js

--- plugins/privacy-tool.js
+++ plugins/privacy-tool.js
@@ -48,7 +48,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363418144382782@newsletter,
+                        newsletterJid: '120363418144382782@newsletter',
                         newsletterName: "Privacy Settings",
                         serverMessageId: 143
                     }
