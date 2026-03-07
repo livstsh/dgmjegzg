e4b2532ba4724632d50766612aafd61e2ad3bf72
livstsh <drkamran185@gmail.com>
2026-03-07 16:43:27 +0500

    Update main-ping.js

--- plugins/main-ping.js
+++ plugins/main-ping.js
@@ -2,23 +2,22 @@ const config = require('../config');
 const { cmd } = require('../command');
 
 const botNameStyles = [
-    "𝙱𝙰𝙶𝙶𝙰-𝚂𝙷𝙴𝙸𝙺𝙷-𝙼𝙳",
-    "𝗕𝗔𝗚𝗚𝗔-𝗦𝗛𝗘𝗜𝗞𝗛-𝗠𝗗",
-    "𝔹𝔸𝔾𝔾𝔸-𝕊ℍ𝔼𝕀𝕂ℍ-𝕄𝔻",
-    "𝑩𝑨𝑮𝑮𝑨-𝑺𝑯𝑬𝑰𝑲𝑯-𝑴𝑫",
-    "🄱🄰🄿🄶🄰-🅂🄷🄴🄸🄺🄷-🄼🄳",
-    "🅱️🅰️🅶🅶🅰️-🆂🅷🅴🅸🅺🅷-🅼🅳",
-    "乃卂ᎶᎶ卂-丂卄乇丨Ҝ卄-爪ᗪ",
-    "ＢＡＧＧＡ-ＳＨＥＩＫＨ-ＭＤ",
-    "🅑🅐🅖🅖🅐-🅢🅗🅔🅘🅚🅗-🅜🅓",
-    "𝓑𝓐𝓖𝓖𝓐-𝓢𝓗𝓔𝓘𝓚𝓗-𝓜𝓓"
+    "𝙱𝙰𝙶𝙶𝙰-𝚂𝙷𝙴𝚁-𝙼𝙳",
+    "𝗕𝗔𝗚𝗚𝗔-𝗦𝗛𝗘𝗥-𝗠𝗗",
+    "𝔹𝔸𝔾𝔾𝔸-𝕊ℍ𝔼ℝ-𝕄𝔻",
+    "𝑩𝑨𝑮𝑮𝑨-𝑺𝑯𝑬𝑹-𝑴𝑫",
+    "🄱🄰🄶🄶🄰-🅂🄷🄴🅁-🄼🄳",
+    "🅱️🅰️🅶🅶🅰️-🆂🅷🅴🆁-🅼🅳",
+    "乃卂ᎶᎶ卂-丂卄乇尺-爪ᗪ",
+    "ＢＡＧＧＡ-ＳＨＥＲ-ＭＤ",
+    "🅑🅐🅖🅖🅐-🅢🅗🅔🅡-🅜🅓",
+    "𝓑𝓐𝓖𝓖𝓐-𝓢𝓗𝓔𝓡-𝓜𝓓"
 ];
 
 let currentStyleIndex = 0;
 
-// Fancy Ping Command
 cmd({
-    pattern: "ping",
+    pattern: "ping4",
     alias: ["speed", "pong"],
     react: "⚡",
     filename: __filename
@@ -31,41 +30,39 @@ cmd({
     let reactionEmoji = reactionEmojis[Math.floor(Math.random() * reactionEmojis.length)];
     let textEmoji = textEmojis[Math.floor(Math.random() * textEmojis.length)];
     
-    // Ensure reaction and text emojis are different
     if (textEmoji === reactionEmoji) {
         textEmoji = textEmojis[(Math.floor(Math.random() * textEmojis.length) + 1) % textEmojis.length];
     }
 
-    await conn.sendMessage(from, { react: { text: textEmoji, key: mek.key } });
+    await conn.sendMessage(from, { react: { text: reactionEmoji, key: mek.key } });
 
     const responseTime = Date.now() - start;
     const fancyBotName = botNameStyles[currentStyleIndex];
     currentStyleIndex = (currentStyleIndex + 1) % botNameStyles.length;
 
     await conn.sendMessage(from, { 
-        text: `> *${fancyBotName} SPEED: ${responseTime}ms ${reactionEmoji}*`,
+        text: `> *${fancyBotName} SPEED: ${responseTime}ms ⚡*`,
         contextInfo: { 
             mentionedJid: [sender],
             forwardingScore: 999,
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
                 newsletterJid: '120363418144382782@newsletter',
-                newsletterName: "𝙱𝙰𝙶𝙶𝙰-𝚂𝙷𝙴𝙸𝙺𝙷-𝙼𝙳 𝚉𝙾𝙽𝙴",
+                newsletterName: "BAGGA-SHER-MD OFFICIAL",
                 serverMessageId: 143
             }
         } 
     }, { quoted: mek });
 });
 
-// Simple Ping Command
 cmd({
     pattern: "ping2",
     react: "🍂",
     filename: __filename
 }, async (conn, mek, m, { from }) => {
     const start = Date.now();
-    const msg = await conn.sendMessage(from, { text: '*🚀 PINGING...*' });
+    const msg = await conn.sendMessage(from, { text: '*PINGING...*' });
     const ping = Date.now() - start;
-    await conn.sendMessage(from, { text: `*BAGGA-SHEIKH-MD SPEED: ${ping}ms*` }, { quoted: msg });
+    await conn.sendMessage(from, { text: `*BAGGA-SHER-MD SPEED: ${ping}ms*` }, { quoted: msg });
 });
-    
+            
