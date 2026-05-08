b20355101b27cd9929780222a9c0186efdbe1e9c
livstsh <drkamran185@gmail.com>
2026-05-08 23:40:48 +0500

    Add files via upload

--- config.js
+++ config.js
@@ -11,9 +11,9 @@ AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
-AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "false",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY PROVA-MD 🇵🇰*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY LUCKY-MD 🇵🇰*",
 // set the auto reply massage on status reply  
 ANTI_DELETE: process.env.ANTI_DELETE || "false",
 // set true false for anti delete     
@@ -29,15 +29,13 @@ MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
 // make true or false for auto voice reply
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/e4za15.jpg",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://uploader.amyuracp.my.id/S1/6UJJPF.jpg",
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "ADEEL-MD",
+BOT_NAME: process.env.BOT_NAME || "LUCKY-MD",
 // add bot namw here for menu
-AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
-// true to get auto status react
-STICKER_NAME: process.env.STICKER_NAME || "PROVA-MD",
+STICKER_NAME: process.env.STICKER_NAME || "LUCKY-MD",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -45,15 +43,15 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923147168309",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "923254368438",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "PROVA-MD",
+OWNER_NAME: process.env.OWNER_NAME || "KUCKY-MD",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴘʀᴏᴠᴀ-ᴍᴅ*",
+DESCRIPTION: process.env.DESCRIPTION || "*©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʟᴜᴄᴋʏ-ᴍᴅ*",
 // add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/e4za15.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://uploader.amyuracp.my.id/S1/6UJJPF.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> I'm alive*PROVA-MD*🇵🇰",
+LIVE_MSG: process.env.LIVE_MSG || "> I'm alive*LUCKY-MD*🇵🇰",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
@@ -77,7 +75,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "923147168309",
+DEV: process.env.DEV || "923254368438",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
